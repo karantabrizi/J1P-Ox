b1fe8791c7997292a8bf932668f13b0e4244db57
karantabrizi <karan.tabrizi@photoninfotech.net>
2015-06-03 10:20:16 -0500

    initialization

---
+++ JCP OSx/JCP OSx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JCP OSx
+//
+//  Created by karan tabrizi on 6/3/15.
+//  Copyright (c) 2015 karan tabrizi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
